f9f9a858618b23e5dbdbfb8d7d785e0842edd538
Andres Vargas <zodman@gmail.com>
2009-08-27 14:34:32 -0500

    add method

--- meteora/__init__.py
+++ meteora/__init__.py
@@ -1,2 +1,2 @@
 from meteora import Meteora
-from utils import json, get_object_or_404
+from utils import *
--- meteora/utils.py
+++ meteora/utils.py
@@ -1,11 +1,16 @@
 from django.utils import simplejson
 from django.http import HttpResponse
 from django.shortcuts import _get_queryset 
+from django.core import serializers
 
 
 def json( convert ):
-    print convert
+    #print convert
     return HttpResponse(simplejson.dumps(convert), mimetype="text/plain")
+def json_object(objectModel):
+    temp = serializers.serialize("json",objectModel)
+    return HttpResponse(temp, mimetype="text/plain")
+    
 
 def get_object_or_404(klass, *args, **kwargs):
     queryset = _get_queryset(klass)
